06c72f0c25174a239a312735e2968eff4ad108d2
Yogalakshmi2005 <149469663+Yogalakshmi2005@users.noreply.github.com>
2025-03-16 13:42:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
 
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/InvokesDB"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
 books_collection = mongo.db.books
 users_collection = mongo.db.users
